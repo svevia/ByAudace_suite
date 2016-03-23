b3ea49fcad0979221762596ab0c292106c4e97e9
valentin KANCURZEWSKI <v.kancurzewski@etudiant.univ-lille1.fr>
2016-03-23 16:57:19 +0100

    Navigation entre activitees

--- Android/app/src/main/java/fr/univ_lille1/iut_info/debaerdm/byaudace/ChoiceActivity.java
+++ Android/app/src/main/java/fr/univ_lille1/iut_info/debaerdm/byaudace/ChoiceActivity.java
@@ -1,9 +1,11 @@
 package fr.univ_lille1.iut_info.debaerdm.byaudace;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class ChoiceActivity extends Activity {
@@ -15,12 +17,14 @@ public class ChoiceActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_choise, menu);
+        getMenuInflater().inflate(R.menu.menu_choice, menu);
         return true;
     }
 
@@ -39,5 +43,21 @@ public class ChoiceActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    public void onClick(View v) {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+
+
+
+    }
+
+    public void onClick2(View v) {
+        Intent intent = new Intent(this, JpeuxAiderActivity.class);
+        startActivity(intent);
+
+
+
+    }
 
 }
