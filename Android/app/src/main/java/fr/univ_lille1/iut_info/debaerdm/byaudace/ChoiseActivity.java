97d4530eb1c133323eee74a68c47508f5c774472
valentin KANCURZEWSKI <v.kancurzewski@etudiant.univ-lille1.fr>
2016-03-23 14:02:27 +0100

    tmp

--- Android/app/src/main/java/fr/univ_lille1/iut_info/debaerdm/byaudace/ChoiseActivity.java
+++ Android/app/src/main/java/fr/univ_lille1/iut_info/debaerdm/byaudace/ChoiseActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class ChoiseActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
