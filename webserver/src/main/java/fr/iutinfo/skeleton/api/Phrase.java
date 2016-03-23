1b3429ab76a44bece10d8630ccf314f346676f0a
aurelia <catricea9@gmail.com>
2016-03-23 16:49:59 +0100

    ajout phrase .java .jsp

---
+++ webserver/src/main/java/fr/iutinfo/skeleton/api/Phrase.java
@@ -0,0 +1,62 @@
+package fr.iutinfo.skeleton.api;
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.security.Principal;
+import java.security.SecureRandom;
+
+public class Phrase implements Principal {
+   // final static Logger logger = LoggerFactory.getLogger(User.class);
+
+    /* Obligatoire */
+    private String phrase;
+    private String besoin;
+    private boolean terminee;
+    private int consultee;
+
+    public Phrase(String phrase, String besoin) {
+        this.phrase = phrase;
+        this.besoin = besoin;
+        this.terminee = false;
+        this.consultee = 0;
+    }
+
+    public String getPhrase() {
+        return this.phrase;
+    }
+
+    public void setPhrase(String phrase) {
+        this.phrase = phrase;
+    }
+
+    public void setBesoin(String besoin) {
+        this.besoin = besoin;
+    }
+
+    public String getBesoin() {
+        return this.besoin;
+    }
+
+    public boolean getTerminee() {
+        return this.terminee;
+    }
+
+    public void setTerminee(boolean terminee) {
+        this.terminee = terminee;
+    }
+
+    @Override
+    public boolean equals(Object arg) {
+        if (getClass() != arg.getClass())
+            return false;
+        Phrase ph = (Phrase) arg;
+        return phrase.equals(ph.phrase) && besoin.equals(ph.besoin);
+    }
+
+    @Override
+    public String toString() {
+        return phrase+ " (" + besoin + ")";
+    }
+}
---
+++ webserver/src/main/java/fr/iutinfo/skeleton/api/PhraseRessource.java
@@ -0,0 +1,50 @@
+package fr.iutinfo.skeleton.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/userdb")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserDBResource {
+	private static UserDao dao = BDDFactory.getDbi().open(UserDao.class);
+    final static Logger logger = LoggerFactory.getLogger(UserDBResource.class);
+
+
+    public UserDBResource() {
+		try {
+			dao.createUserTable();
+			dao.insert(new User(0,"Margaret Thatcher", "la Dame de fer"));
+		} catch (Exception e) {
+			System.out.println("Table déjà là !");
+		}
+	}
+	
+	@POST
+	public User createUser(User user) {
+        user.resetPasswordHash();
+        int id = dao.insert(user);
+        user.setId(id);
+		return user;
+	}
+
+	@GET
+	@Path("/{name}")
+	public User getPhrase(@PathParam("name") String name) {
+		Phrase ph = dao.findByName(name);
+		if (ph == null) {
+			throw new WebApplicationException(404);
+		}
+		return ph;
+	}
+
+	@GET
+	public List<Phrase> getAllPhrase() {
+		return dao.all();
+	}
+
+}
