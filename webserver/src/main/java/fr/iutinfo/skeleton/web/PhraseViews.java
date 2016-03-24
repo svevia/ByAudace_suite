cd6523c88dd07a8fbc1c16cc871e09dce333995b
Aurelia Catrice <catricea9@gmail.com>
2016-03-24 17:19:02 +0100

    màj

---
+++ webserver/src/main/java/fr/iutinfo/skeleton/web/PhraseViews.java
@@ -0,0 +1,33 @@
+package fr.iutinfo.skeleton.web;
+
+import fr.iutinfo.skeleton.api.BDDFactory;
+import fr.iutinfo.skeleton.api.*;
+import org.glassfish.jersey.server.mvc.Template;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/phrase")
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+@Produces(MediaType.TEXT_HTML)
+public class PhraseViews {
+    private static PhraseDao dao = BDDFactory.getDbi().open(PhraseDao.class);
+
+    @GET
+    @Template
+    public List<Phrase> getAllPhrase() {
+        return dao.all();
+    }
+
+    @GET
+    @Template(name = "detail")
+    @Path("/{name}")
+    public Phrase getPhrase(@PathParam("name") String name) {
+        Phrase ph = dao.findByName(name);
+        if (ph == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+        return ph;
+    }
+}
