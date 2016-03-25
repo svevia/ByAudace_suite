0c9343285aa74d9c9e1da46794fcd21d08f35fc0
maugern <n-mauger@hotmail.fr>
2016-03-25 10:58:19 +0100

    edit test

--- webserver/src/test/java/fr/iutinfo/skeleton/api/UserDBResourceTest.java
+++ webserver/src/test/java/fr/iutinfo/skeleton/api/UserDBResourceTest.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Before;
@@ -24,17 +27,17 @@ public class UserDBResourceTest extends JerseyTest{
         h = new Helper(target("/userdb"));
         h.initDb();
     }
-
+    
     @Test
     public void should_return_user_as_object() {
-    	h.createUser("Clavier", "Thomas", "tc@gmail.com", "1234pouet", "AZERTY", "user");
-    	User utilisateur = target("/userdb/").request().get(User.class);
-    	assertEquals("tc@gmail.com", utilisateur.getMail());
+    	h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
+    	User user = target("/userdb/Clavier").request().get(User.class);
+    	assertEquals("Clavier", user.getNom());
     }
 
     @Test
     public void should_return_all_users() {
-    	h.createUser("Clavier", "Thomas", "tc@gmail.com", "1234pouet", "AZERTY", "user");
+    	h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
     	h.createUser("Seq", "Yann", "ys@gmail.com", "azerty123", "OUIOUI", "admin");
         List<User> users = target("/userdb").request().get(new GenericType<List<User>>() {});
         assertEquals(2, users.size());
@@ -42,15 +45,15 @@ public class UserDBResourceTest extends JerseyTest{
 
     @Test
     public void list_all_must_be_ordered() {
-    	h.createUser("Clavier", "Thomas", "tc@gmail.com", "1234pouet", "AZERTY", "user");
-    	h.createUser("Seq", "Yann", "ys@gmail.com", "azerty123", "OUIOUI", "admin");
+    	h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
+    	h.createUser("ys@gmail.com", "Seq", "Yann", "OUIOUI", "azerty123", "admin");
         List<User> users = target("/userdb").request().get(new GenericType<List<User>>() {});
-        assertEquals("tc@gmail.com", users.get(0).getMail());
+        assertEquals("Clavier", users.get(0).getNom());
     }
 
     @Test
     public void testReadUserWithNameFooAsJsonString() {
-    	h.createUser("Clavier", "Thomas", "tc@gmail.com", "1234pouet", "AZERTY", "user");
+    	h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
         String json = target("/userdb").request().get(String.class);
         assertTrue(json.contains("\"mail\":\"tc@gmail.com\""));
     }
@@ -63,24 +66,23 @@ public class UserDBResourceTest extends JerseyTest{
 
     @Test
     public void testCreateUserMustReturnUserWithMail() {
-        User savedUser = h.createUser("Clavier", "Thomas", "tc@gmail.com", "1234pouet", "AZERTY", "user");
-        assertFalse(savedUser.getMail().equals(""));
+        User savedUser = h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
+        assertFalse(savedUser.getMail().equals(null));
     }
 
     @Test
     public void testUpdateUserName() {
-        User u = h.createUser("Clavier", "Thomas", "tc@gmail.com", "1234pouet", "AZERTY", "user");
+        User u = h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
         u.setPrenom("yann");
-        Response rep = target("/userdb").path("" + u.getNom.request()
-                .put(Entity.entity(u, MediaType.APPLICATION_JSON));
+        Response rep = target("/userdb").path("" + u.getPrenom()).request().put(Entity.entity(u, MediaType.APPLICATION_JSON));
         User updatedUser = rep.readEntity(User.class);
-        assertEquals("yann", updatedUser.getNom());
+        assertEquals("yann", updatedUser.getPrenom());
     }
 
     @Test
     public void testGetingSameUserTwice() {
-        User user1 = target("/userdb/mail").request().get(User.class);
-        User user2 = target("/userdb/mail").request().get(User.class);
+        User user1 = h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
+        User user2 = h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
         assertEquals(user1.toString(), user2.toString());
     }
 
@@ -92,18 +94,24 @@ public class UserDBResourceTest extends JerseyTest{
 
     @Test
     public void tesListAllUsers() {
-		h.createUser("Clavier", "Thomas", "tc@gmail.com", "1234pouet", "AZERTY", "user");
+    	h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
     	h.createUser("Seq", "Yann", "ys@gmail.com", "azerty123", "OUIOUI", "admin");
-        List<User> users = target("/userdb/").request().get(new GenericType<List<User>>() {});
+        List<User> users = target("/userdb").request().get(new GenericType<List<User>>() {});
         assertTrue(users.size() >= 2);
     }
 
     @Test
     public void after_delete_read_user_sould_return_202() {
-        User u = h.createUser("Clavier", "Thomas", "tc@gmail.com", "1234pouet", "AZERTY", "user");
+        User u = h.createUser("tc@gmail.com", "Clavier", "Thomas", "AZERTY", "1234pouet", "user");
         int status = target("/userdb/" + u.getMail()).request().delete().getStatus();
         assertEquals(202, status);
-
+    }
+    
+    @Test
+    public void should_set_salt_at_build () {
+        User user = new User();
+        assertNotNull(user.getSalt());
+        assertFalse(user.getSalt().isEmpty());
     }
 
 }
