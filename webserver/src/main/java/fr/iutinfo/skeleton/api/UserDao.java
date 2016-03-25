4c099567136cfeb1c2c6c7c6b1b58bfe78767e66
Nicolas Seys <seys.nicolas@gmail.com>
2016-03-25 09:16:55 +0100

    create user qui marche

--- webserver/src/main/java/fr/iutinfo/skeleton/api/UserDao.java
+++ webserver/src/main/java/fr/iutinfo/skeleton/api/UserDao.java
@@ -2,20 +2,14 @@ package fr.iutinfo.skeleton.api;
 
 import java.util.List;
 import org.skife.jdbi.v2.sqlobject.*;
-import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
 import org.skife.jdbi.v2.tweak.BeanMapperFactory;
 
 public interface UserDao {
 
-	/*@SqlUpdate("insert into utilisateur (mail, nom, prenom, digit, mot_de_passe, role) values (:mail, :nom, :prenom, :digit, :mot_de_passe, :role)")
-	//@GetGeneratedKeys
-	void insert(@BindBean() User user);*/
-
-        @SqlUpdate("insert into utilisateur (mail, nom, prenom, digit, mot_de_passe, role) values (:mail, :nom, :prenom, :digit, :mot_de_passe, :role)")
+	@SqlUpdate("insert into utilisateur (mail, nom, prenom, digit, mot_de_passe, role) values (:mail, :nom, :prenom, :digit, :mot_de_passe, :role)")
 	@GetGeneratedKeys
-	int insert(@Bind("mail") String mail, @Bind("nom") String nom, @Bind("prenom") String prenom, 
-            @Bind("digit") String digit, @Bind("mot_de_passe") String mot_de_passe, @Bind("role") String role);
+	int insert(@BindBean() User user);
     
 	@SqlQuery("select * from utilisateur where mail = :mail")
         @RegisterMapperFactory(BeanMapperFactory.class)
