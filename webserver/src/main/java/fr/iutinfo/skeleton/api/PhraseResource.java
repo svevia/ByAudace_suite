3111105d776b7b1488fc11db9c5050c706a6dba2
Nicolas Seys <seys.nicolas@gmail.com>
2016-03-25 10:10:12 +0100

    updated phrase request for insert and delete

--- webserver/src/main/java/fr/iutinfo/skeleton/api/PhraseResource.java
+++ webserver/src/main/java/fr/iutinfo/skeleton/api/PhraseResource.java
@@ -13,38 +13,43 @@ import java.util.List;
 
 public class PhraseResource {
 
-	private static PhraseDao dao = BDDFactory.getDbi().open(PhraseDao.class);
+    private static PhraseDao dao = BDDFactory.getDbi().open(PhraseDao.class);
     final static Logger logger = LoggerFactory.getLogger(PhraseResource.class);
+    
+    @POST
+    public Phrase createPhrase(Phrase phrase) {
+        //System.out.println("Create phrase : phrase=" + phrase.getPhrase()
+        //    + ", besoin=" + phrase.getBesoin() + ", mail_deposant=" + phrase.getMail_deposant()
+        //    + ", terminee=" + phrase.getTerminee() + ", consultee=" + phrase.getConsultee());
+        dao.insert(phrase);
+        return phrase;
+    }
 
+    @GET
+    @Path("/{phrase}")
+    public Phrase getPhrase(@PathParam("phrase") String phrase) {
+        Phrase ph = dao.findByPhrase(phrase);
+        if (ph == null) {
+            throw new WebApplicationException(404);
+        }
+        return ph;
+    }
+    
+    @DELETE
+    @Path("/{phrase}")
+    public Phrase deleteUser(@PathParam("phrase") String phrase) {
+        Phrase ph = dao.findByPhrase(phrase);
+        System.out.println("Deleting phrase : " + phrase);
+        if (ph == null) {
+            throw new WebApplicationException(404);
+        }
+        dao.delete(phrase);
+        return ph;
+    }
 
-  /*  public PhraseResource() {
-		try {
-			dao.createPhraseTable();
-		} catch (Exception e) {
-			System.out.println("Table déjà là !");
-		}
-	}*/
-	
-	@POST
-	public Phrase createPhrase(String name) {
-        Phrase ph = dao.insert(name);
-        ph.setPhrase(name);
-		return ph;
-	}
-
-	@GET
-	@Path("/{name}")
-	public Phrase getPhrase(@PathParam("name") String name) {
-		Phrase ph = dao.findByName(name);
-		if (ph == null) {
-			throw new WebApplicationException(404);
-		}
-		return ph;
-	}
-
-	@GET
-	public List<Phrase> getAllPhrase() {
-		return dao.all();
-	}
+    @GET
+    public List<Phrase> getAllPhrase() {
+        return dao.all();
+    }
 
 }
