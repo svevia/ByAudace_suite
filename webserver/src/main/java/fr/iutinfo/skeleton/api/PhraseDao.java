ecc2d1b142b3ab9b7d0732f7a73832905cf8bd8e
catricea <catricea9@gmail.com>
2016-04-01 00:28:03 +0200

    commentaire phraseDao.java

--- webserver/src/main/java/fr/iutinfo/skeleton/api/PhraseDao.java
+++ webserver/src/main/java/fr/iutinfo/skeleton/api/PhraseDao.java
@@ -10,32 +10,34 @@ import org.skife.jdbi.v2.tweak.BeanMapperFactory;
  * @author seysn
  */
 public interface PhraseDao {
-
+    //insertion d'une phrase (methode post)
     @SqlUpdate("insert into phrase_metier (phrase, besoin, mail, terminee, consultee) values (:phrase, :besoin, :mail, :terminee, :consultee)")
     @GetGeneratedKeys
     int insert(@BindBean() Phrase phrase);
-
+    //selection d'une phrase en fonction de son intitule "phrase" (methode get)
     @SqlQuery("select * from phrase_metier where phrase = :phrase")
     @RegisterMapperFactory(BeanMapperFactory.class)
     Phrase findByPhrase(@Bind("phrase") String phrase);
-
+    //suppression d'une phrase en fonction de son intitule "phrase" (utilisee dans PhraseViews/index.jsp methode delete)
     @SqlUpdate("delete from phrase_metier where phrase = :phrase")
     void delete(@Bind("phrase") String phrase);
-
+    //selection de toutes les phrases (utilisee dans PhraseViews/index.jsp methode get)
     @SqlQuery("select * from phrase_metier")
     @RegisterMapperFactory(BeanMapperFactory.class)
     List<Phrase> all();
-    
+    //retourne le nombre total de phrases (utilisee dans StatistiqueViews/index.jsp methode get)
     @SqlQuery("select count(*) from phrase_metier")
     int getAllCount();
-    
+    //retourne le nombre total de phrases terminees (utilisee dans StatistiqueViews/index.jsp methode get)
     @SqlQuery("select count(*) from phrase_metier where terminee = :bool")
     int getTermCount(@Bind("bool") boolean bool);
-
+    //selectionne les phrases correspondant a la recherche (utilisee dans PhraseViews/index.jsp methode get)
     @SqlQuery("select * from phrase_metier where mail like :search "
             + "or besoin like :search or phrase like :search")
     @RegisterMapperFactory(BeanMapperFactory.class)
     List<Phrase> search(@Bind("search") String search);
+    
+    //selectionne les phrases triees par champs (utilisee dans PhraseViews/index.jsp methode get)
     
     @SqlQuery("select * from phrase_metier order by besoin")
     @RegisterMapperFactory(BeanMapperFactory.class)
